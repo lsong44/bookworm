2d0b79fde6bcd0bbb84793db49c9e4d6afd8e235
lsong44 <78609821+lsong44@users.noreply.github.com>
2025-01-11 15:22:22 -0600

    added cors configurer (#9)
    * updated pipeline and database config
    
    * removed invalid option
    
    * added cors configurer

---
+++ mainProj/src/main/java/com/li/bookworm/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.li.bookworm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    // needed to allow api calls made from any origin, postman included
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
