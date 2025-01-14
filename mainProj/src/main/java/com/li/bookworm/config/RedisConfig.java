fb94f6730773dbf70af236319e9fed0df3ec40ab
Li Song <lisongcaltech@gmail.com>
2025-01-14 11:25:41 -0600

    enabled redis cache for group controller

---
+++ mainProj/src/main/java/com/li/bookworm/config/RedisConfig.java
@@ -0,0 +1,51 @@
+package com.li.bookworm.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Value("${spring.redis.ssl}")
+    private boolean redisSsl;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHost, redisPort);
+        lettuceConnectionFactory.setPassword(redisPassword);
+        lettuceConnectionFactory.setUseSsl(redisSsl);
+        return lettuceConnectionFactory;
+    }
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        return template;
+    }
+
+}
---
+++ mainProj/src/main/java/com/li/bookworm/constants/CacheKeyConstants.java
@@ -0,0 +1,8 @@
+package com.li.bookworm.constants;
+
+public class CacheKeyConstants {
+    public static final String GROUP_ALL_CACHE_KEY = "groups";
+    public static final String MEMBER_ALL_CACHE_KEY = "members";
+    public static final String LOG_ALL_CACHE_KEY = "logs";
+    public static final String MEMBERSHIP_ALL_CACHE_KEY = "memberships";
+}
