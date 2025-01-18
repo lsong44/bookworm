884d0bff2a39a671326dcde539a82d9c3315b792
Li Song <lisongcaltech@gmail.com>
2025-01-17 21:30:14 -0600

    integrated tests in pipeline

--- IntegrationTests/src/test/java/com/li/bookworm/runner/IntegrationTest.java
+++ IntegrationTests/src/test/java/com/li/bookworm/runner/IntegrationTest.java
@@ -6,5 +6,5 @@ import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features", glue = { "classpath:com.li.bookworm.stepdefs" }, tags = "@BookwormTest", plugin = { "pretty", "junit:target/cucumber-reports/bookworm-test-report.xml" })
-public class AppIntegrationTestsRunner {
+public class IntegrationTest {
 }
