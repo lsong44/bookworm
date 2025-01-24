6c19f7ee7b38eecc0e898daa3a466dd9c015c67e
Li Song <lisongcaltech@gmail.com>
2025-01-23 20:55:08 -0600

    added oauth

---
+++ mainProj/src/main/java/com/li/bookworm/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.li.bookworm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String[] AUTH_ALLOWLIST = {"/swagger-ui.html", "/swagger-ui/**", "/api/*"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .csrf(Customizer.withDefaults())
+                .httpBasic(withDefaults())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
